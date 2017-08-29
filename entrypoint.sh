7b7b25ef11037f83e0f7b3b7ccf20e0b02616317
Fabio <fabio.nitto@planejamento.gov.br>
2017-08-29 18:00:12 -0300

    Added option to change CPU count]

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 
 sysctl -w vm.nr_hugepages=128
 
+for i in `seq 0 $(($CPU-1))` 
+do
+    CPUS=$CPUS"{ \"low_power_mode\" : false, \"no_prefetch\" : true, \"affine_to_cpu\" : $i },\n"
+done
+
+sed -i -e "s/\${CPU}/$CPUS/g" /xmr-stak-cpu/bin/config.txt
 sed -i -e "s/\${POOL}/$POOL/g" /xmr-stak-cpu/bin/config.txt
 sed -i -e "s/\${ADDRESS}/$ADDRESS/g" /xmr-stak-cpu/bin/config.txt
 sed -i -e "s/\${PORT}/$PORT/g" /xmr-stak-cpu/bin/config.txt
